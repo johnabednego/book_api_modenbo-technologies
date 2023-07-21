c00c5ee5744b5f8b302e9187d6d157e6cc22a7ce
johnabednego <jajilima@st.ug.edu.gh>
2023-07-21 05:52:01 +0000

    done

--- book_api/settings.py
+++ book_api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#3#8uq&!kd35#%5u@^u1!31(e8e!(s&4ymo73%7@b-^ed7_kty
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
