8afd038d165e3451527b069da3cdb50cc36b31c5
johnabednego <jajilima@st.ug.edu.gh>
2023-07-21 22:59:26 +0000

    setting.py

--- book_api/urls.py
+++ book_api/urls.py
@@ -16,12 +16,16 @@ Including another URLconf
 """
 
 # book_api/urls.py
-
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),  # Include the Django Admin URLs
     path('api/', include('books.urls')),  # Include the API endpoints from the books app
 ]
 
+# Serve media files during development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
