7fa92ee0fe78c3e7750e84e4fb2c33b137eda43c
johnabednego <jajilima@st.ug.edu.gh>
2023-07-22 01:44:40 +0000

    finally done

--- books/serializers.py
+++ books/serializers.py
@@ -4,6 +4,7 @@ from rest_framework import serializers
 from .models import Book
 
 class BookSerializer(serializers.ModelSerializer):
+    image = serializers.ImageField(use_url=True)  
     class Meta:
         model = Book
         fields = '__all__'
